8060a318892573d161305c9a2a32c5fb4f970763
Ecobrieduque <ecobrieduque@gmail.com>
2024-10-24 09:13:52 -0300

    Delete css directory

--- css/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var menuToggle = document.getElementById('menu-toggle');
-  var menu = document.querySelector('.menu');
-
-  menuToggle.addEventListener('click', function () {
-      menu.classList.toggle('show');
-  });
-});
